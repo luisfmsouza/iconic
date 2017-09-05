27802e11059c3cacf2f06acb0e2149e77192ed57
Luís Felipe Souza <luisfmsouza@gmail.com>
2017-09-04 22:19:27 -0300

    Fix xo issues

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import * as svgicon from 'vue-svgicon'
-import './assets/icons/airplane'
-import './assets/icons/beer'
-import './assets/icons/heart'
+import Vue from 'vue';
+import * as svgicon from 'vue-svgicon';
+import App from './App.vue';
+import './assets/icons/airplane';
+import './assets/icons/beer';
+import './assets/icons/heart';
 
 Vue.use(svgicon, {
   tagName: 'icon'
-})
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
