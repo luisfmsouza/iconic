6254298ba5f633d808550072a29800f0555970a0
Luís Felipe Souza <luisfmsouza@gmail.com>
2017-09-04 23:27:36 -0300

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,4 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
