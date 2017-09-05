20836987d1dae0bee0dc6a3328ff32e361fbc57e
Luís Felipe Souza <luisfmsouza@gmail.com>
2017-09-04 22:04:31 -0300

    Import icons on main app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as svgicon from 'vue-svgicon'
+import './assets/icons/airplane'
+import './assets/icons/beer'
+import './assets/icons/heart'
+
+Vue.use(svgicon, {
+  tagName: 'icon'
+})
 
 new Vue({
   el: '#app',
